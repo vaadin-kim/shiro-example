264d7fbb3a0c72bfb378fed0bb1772d406704b38
Kim Leppänen <kimleppanen@guest-192-168-6-138.guestlan.itmill.com>
2013-09-25 14:23:47 +0300

    ViewChangeListener to block access to view

--- shiro-login-within-vaadin-app/src/main/java/org/vaadin/example/shiro/ExampleUI.java
+++ shiro-login-within-vaadin-app/src/main/java/org/vaadin/example/shiro/ExampleUI.java
@@ -2,13 +2,17 @@ package org.vaadin.example.shiro;
 
 import javax.servlet.annotation.WebServlet;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
 
-public class ExampleUI extends UI {
+public class ExampleUI extends UI implements ViewChangeListener {
 
 	@WebServlet(value = "/*")
 	@VaadinServletConfiguration(productionMode = false, ui = ExampleUI.class)
@@ -18,9 +22,34 @@ public class ExampleUI extends UI {
 	@Override
 	protected void init(VaadinRequest request) {
 		Navigator navigator = new Navigator(this, this);
+		navigator.addViewChangeListener(this);
 
 		navigator.addView("", LoginView.class);
 		navigator.addView("secure", SecureView.class);
+	}
+
+	@Override
+	public boolean beforeViewChange(ViewChangeEvent event) {
+		Subject currentUser = SecurityUtils.getSubject();
+		if (currentUser.isAuthenticated()
+				&& event.getViewName().equals("")) {
+			event.getNavigator().navigateTo("secure");
+			return false;
+		}
+
+		if (!currentUser.isAuthenticated()
+				&& !event.getViewName().equals("")) {
+			event.getNavigator().navigateTo("");
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public void afterViewChange(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
